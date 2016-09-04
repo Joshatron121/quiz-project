cf2a04c849bc3a75dba1e5d0683db0784e5333ff
Josh Eimer <jeimer@Joshs-MacBook-Pro.local>
2016-09-04 04:06:29 -0500

    fixed layout issues, added Quiz object

--- js/app.js
+++ js/app.js
@@ -1,3 +1,90 @@
 $(function(){
+	var Quiz = {
+		questionsArray: [
+			{
+				questionNum: 1,
+				question: 'Where was the original Stargate discovered on Earth?',
+				answerA: 'New Brunswick',
+				answerB: 'Antarctica',
+				answerC: 'The Bottom of the Ocean',
+				answerD: 'Egypt'
+			},
+			{
+				questionNum: 2,
+				question: 'What was the name of the first Goa’uld a team from Earth has to deal with?',
+				answerA: 'Apophis',
+				answerB: 'Ra',
+				answerC: 'Ba\'al',
+				answerD: 'Anubis'
+			},
+			{
+				questionNum: 3,
+				question: 'What was the name of the first planet visited by a team of explorers from Earth?',
+				answerA: 'Chulak',
+				answerB: 'Abydos',
+				answerC: 'Tollana',
+				answerD: 'Dakara'
+			},
+			{
+				questionNum: 4,
+				question: 'What is the name of the team member who was originally the First Prime of Apophis?',
+				answerA: 'Jack O\'Neill',
+				answerB: 'Samantha Carter',
+				answerC: 'Daniel Jackson',
+				answerD: 'Teal\'c'
+			},
+			{
+				questionNum: 5,
+				question: 'What was the name of Earth’s first battlecruiser, also known as the X-303?',
+				answerA: 'Deadalus',
+				answerB: 'Odyssey',
+				answerC: 'Prometheus',
+				answerD: 'Apollo'
+			},
+			{
+				questionNum: 6,
+				question: 'How many Stargate TV Shows have there been?',
+				answerA: '1',
+				answerB: '2',
+				answerC: '3',
+				answerD: '4'
+			},
+			{
+				questionNum: 7,
+				question: 'Where was the second Stargate on Earth found?',
+				answerA: 'New Brunswick',
+				answerB: 'Antarctica',
+				answerC: 'The Bottom of the Ocean',
+				answerD: 'Egypt'
+			},
+			{
+				questionNum: 8,
+				question: 'In the episode “Window of Opportunity”, while stuck in a time loop, what did Jack O’Neill resign his command to do?',
+				answerA: 'Golf into the open wormhole of a Stargate',
+				answerB: 'Punch Maybourne',
+				answerC: 'Kiss Samantha Carter',
+				answerD: 'Learn to juggle with Teal\'c'
+			},
+			{
+				questionNum: 9,
+				question: 'Daniel Jackson was the only SG-1 team member to do what?',
+				answerA: 'Die',
+				answerB: 'Ascend',
+				answerC: 'Dance with Teal\'c',
+				answerD: 'Be resusciatated'
+			},
+			{
+				questionNum: 10,
+				question: 'What is the name of the good Goa’uld symbiotes that oppose the oppression of the System Lords?',
+				answerA: 'Tokra',
+				answerB: 'Tau\'ri',
+				answerC: 'Tollan',
+				answerD: 'The Wraith'
+			}
+		],
+		answersArray: [4, 2, 2, 4, 3, 3, 2, 3, 2, 1],
+		totalCorrect: 0,
+	}
+
 	
 });
